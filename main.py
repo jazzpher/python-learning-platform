682fa1529f47fe29d09a5167acf9ae3947522bad
jazzauguis <29362922-jazzauguis@users.noreply.replit.com>
2024-08-05 08:41:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/lesson/python-basics')
+def python_basics():
+    return render_template(
+        'lesson.html',
+        lesson_title="Python Basics",
+        lesson_content="Here's where you'd put your lesson content.")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
